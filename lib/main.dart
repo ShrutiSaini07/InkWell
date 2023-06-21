d74fdf550538020914b911abfdb715d80ecba614
Hp <shrutisaini1415@gmail.com>
2023-06-22 02:20:56 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('INKWELL'),
+      ),
+      body: Center(
+        child: InkWell(
+          onTap: (){
+            print('Hoiiiiiii!!');
+          },
+          child: Container(
+            width: 200,
+            height: 200,
+            color: Colors.amber,
+            child: Center(
+              child: InkWell(
+              onTap:(){
+              print('Text Widgets');
+              },
+                child: Text(
+                  'Click Here',style:TextStyle(fontSize: 21,fontWeight: FontWeight.w700),
+                ))),
+          ),
+        ),
+      )
+    );
+  }
+}
